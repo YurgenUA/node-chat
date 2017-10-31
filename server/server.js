d1555de1b4eec7385eb3f8f4d8cbc0ae36b8c99d
YurgenUA <yuri.ukraine@gmail.com>
2017-10-31 23:29:57 +0200

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
 const express = require('express')
-const app = express();
+const socketIO = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+const app = express();
+const server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log('New connection');
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    })
+});
+
 app.use(express.static(publicPath))
 
-app.listen(port, function () {
+server.listen(port, function () {
     console.log(`Example app listening on port ${port}!`)
 });
 
